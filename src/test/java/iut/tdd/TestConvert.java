95749cf41888df9ce0821ab84a2542977e24f2cb
jonathan delbart <jonathan delbart>
2015-04-17 14:52:03 +0200

    commit1

--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -6,15 +6,13 @@ import org.junit.Test;
 
 public class TestConvert {
 	@Test
-	public void test_num2text_zero () {
-		// Given
-		String input = "0";
-		String expected = "zéro";
-		Convert myConvert = new Convert();
-		// When
-		String actual = myConvert.num2text(input);
-		// Then
-		Assert.assertEquals(expected, actual);
+	public void test_num2text_zero() {
+		Assert.assertEquals("zéro", "0");
 	}
-	
+
+	@Test
+	public void test_num2text_onze() {
+		Assert.assertEquals("onze", "11");
+
+	}
 }
