cd0594452026f5ef9df6020e5c0be905fbabf9ee
jonathan delbart <jonathan delbart>
2015-04-17 17:20:04 +0200

    commit4

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -25,8 +25,6 @@ public class Convert {
 			return "huit";
 		case "9":
 			return "neuf";
-		case "10":
-			return "dix";
 		case "11":
 			return "onze";
 		case "12":
@@ -40,50 +38,140 @@ public class Convert {
 		case "16":
 			return "seize";
 
-		case "17":
-			return "dix-sept";
-		case "18":
-			return "dix-huit";
-		case "19":
-			return "dix-neuf";
-		case "20":
-			return "vingt";
 		}
 		return null;
 
 	}
-	
-	public static  String num2virgule(String input){
-		int length = input.length();
-	 if(input.charAt(1)== '.'){
-			return zero2twenty("" + input.charAt(0)) + " virgule " + zero2twenty("" + input.charAt(2));
+
+	public static String Dizaine(String input) {
+		switch (input) {
+		case "10":
+			return "dix";
+		case "20":
+			return "vingt";
+		case "30":
+			return "trente";
+		case "40":
+			return "quarante";
+		case "50":
+			return "cinquante";
+		case "60":
+			return "soixante";
+		case "70":
+			return "soixante-dix";
+		case "80":
+			return "quatre-vingt";
+		case "90":
+			return "quatre-vingt-dix";
+
 		}
-		
 		return null;
-		
+
+	}
+
+	public static String num2virgule(String input) {
+		int length = input.length();
+		if (input.charAt(1) == '.') {
+			return zero2twenty("" + input.charAt(0)) + " virgule "
+					+ zero2twenty("" + input.charAt(2));
+		}
+
+		return null;
+
 	}
 
 	public static String num2text(String input) {
 		int length = input.length();
-		if(zero2twenty(input)==null){
-			if(num2virgule(input)==null){
-		switch(length){
-		case 2: switch(input.charAt(0)){
-		case '2' :  return "vingt-" + num2text("" + input.charAt(1));
-		case '3' : return "trente-" + num2text("" + input.charAt(1));
-		case '4' : return "quarante-" + num2text ("" + input.charAt(1));
-		case '5' : return "cinquante-" + num2text ("" + input.charAt(1));
-		case '6' : return "soixante-" + num2text ("" + input.charAt(1));
-		case '8' : return "quatre-vingt-" + num2text ("" + input.charAt(1));
-		}
-		}
-		}
-			else{ return num2virgule(input);}
+		if (zero2twenty(input) == null) {
+			if (num2virgule(input) == null) {
+				switch (length) {
+				case 2:
+					if (input.charAt(1) == '1') {
+						switch (input.charAt(0)) {
+						case '1':
+							return Dizaine("10") + " et " + zero2twenty("1");
+						case '2':
+							return Dizaine("20") + " et " + zero2twenty("1");
+						case '3':
+							return Dizaine("30") + " et " + zero2twenty("1");
+						case '4':
+							return Dizaine("40") + " et " + zero2twenty("1");
+						case '5':
+							return Dizaine("50") + " et " + zero2twenty("1");
+						case '6':
+							return Dizaine("60") + " et " + zero2twenty("1");
+						case '7':
+							return Dizaine("60") + " et " + zero2twenty("11");
+						case '8':
+							return Dizaine("80") + "-" + zero2twenty("1");
+						case '9':
+							return Dizaine("90")  + "-" + zero2twenty("11");
+						}
+					}
+
+					else if (input.charAt(1) == '0') {
+						switch (input.charAt(0)) {
+						case '1':
+							return Dizaine("10");
+						case '2':
+							return Dizaine("20");
+						case '3':
+							return Dizaine("30");
+						case '4':
+							return Dizaine("40");
+						case '5':
+							return Dizaine("50");
+						case '6':
+							return Dizaine("60");
+						case '7':
+							return Dizaine("70");
+						case '8':
+							return Dizaine("80");
+						case '9':
+							return Dizaine("90");
+
+						}
+
+					} else {
+						switch (input.charAt(0)) {
+						case '1':
+							return Dizaine("10") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '2':
+							return Dizaine("20") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '3':
+							return Dizaine("30") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '4':
+							return Dizaine("40") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '5':
+							return Dizaine("50") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '6':
+							return Dizaine("60") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '7':
+							return Dizaine("70") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '8':
+							return Dizaine("80") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						case '9':
+							return Dizaine("90") + "-"
+									+ zero2twenty("" + input.charAt(1));
+						}
+					}
+				}
+			} else {
+				return num2virgule(input);
+			}
 		}
 
-		else{ return zero2twenty(input);}
-		
-		
+		else {
+			return zero2twenty(input);
+		}
 
 		return null;
 	}
